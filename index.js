eeb5a04ce4a581b23d57a764cd15ff8e8c90b26f
T'Vedt Lazenby <tvedt@10downproductions.com>
2022-08-23 10:30:37 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let movieArray = ('movieArray')
+
+document.addEventListener('DOMContentLoaded', function() {
+    // code here will execute after the document is loaded
+    });
+
+    function renderMovies(movieArray){
+
+    }
+
+    
